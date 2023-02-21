995c87d90acfc7a42a2f35e09350e6cb05e7ae1e
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-02-21 20:40:49 +0100

    Add search handler

--- src/app.py
+++ src/app.py
@@ -5,3 +5,10 @@ app = f.Flask(__name__)
 @app.route("/")
 def home():
     return f.render_template("home.html")
+
+@app.route("/search")
+def search():
+    q = f.request.args.get('q')
+    index = f.request.args.getlist('index')
+    sort = f.request.args.get('sort')
+    return f"Query: {q}\nIndices:{index}\nSort:{sort}"
