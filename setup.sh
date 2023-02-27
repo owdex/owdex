51ba9ffab1cec448df8c850b632311b91e738532
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-02-27 16:51:27 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# xinase setup script for easy configuration. creates the 'data' folders that solr will look for.
+
+if [ "$EUID" -ne 0 ]; then 
+    echo "xinase setup should be run as root in order to transfer ownership of the data folder correctly"
+    exit
+fi
+
+mkdir data data/data data/logs
+echo "successfully made folder structure"
+chown -R 8983 data 
+echo "solr folders now owned by 8983:8983"
+
+echo "done :)"
