0a807d7721f530e0bbc38925decebbf3330dea6e
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-01 21:20:06 +0100

    set up single shared schema for all indices, dockerise

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
-# owdex setup script for easy configuration. creates the 'data' folders that solr will look for.
+# owdex setup script for easy configuration
 
 if [ "$EUID" -ne 0 ]; then 
     echo "owdex setup should be run as root in order to transfer ownership of the data folder correctly"
     exit
 fi
 
+echo "making data folder structure"
 mkdir data data/data data/logs
-echo "successfully made folder structure"
 chown -R 8983 data 
-echo "solr folders now owned by 8983:8983"
+echo "initialising solr"
+docker-compose up -d solr
+sleep 3
+docker-compose down
+echo "linking solr configuration and schema files"
+# obviously, /config/schema.xml doesn't exist on the host.
+# it's the mount point for the files in the container as defined by docker-compose.yml
+# so these links will be broken on the host but work fine in the container :)
+ln -sf /config/schema.xml data/data/stable/conf/schema.xml
+ln -sf /config/schema.xml data/data/unstable/conf/schema.xml
+ln -sf /config/schema.xml data/data/archive/conf/schema.xml
+ln -sf /config/solrconfig.xml data/data/stable/conf/solrconfig.xml
+ln -sf /config/solrconfig.xml data/data/unstable/conf/solrconfig.xml
+ln -sf /config/solrconfig.xml data/data/archive/conf/solrconfig.xml
+
 
 echo "done :)"
