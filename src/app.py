4d39acc9186725ac05c5630d0c64a50c729f56be
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-02 20:00:55 +0100

    Add search

--- src/app.py
+++ src/app.py
@@ -60,13 +60,9 @@ def search():
     query = f.request.args.get('query')
     indices = f.request.args.getlist('index')
     sort = f.request.args.get('sort')
-    results = [
-        {"url":"https://example.com","title":"An example webpage","extract":"This is an extract from an example webpage."},
-        {"url":"https://example.com","title":"An example webpage","extract":"This is an extract from an example webpage."},
-        {"url":"https://example.com","title":"An example webpage","extract":"This is an extract from an example webpage."},
-        {"url":"https://example.com","title":"An example webpage","extract":"This is an extract from an example webpage."},
-        {"url":"https://example.com","title":"An example webpage","extract":"This is an extract from an example webpage."}
-    ]
+
+    results = db["unstable"].search(query)
+
     return f.render_template("search.html", query=query, indices=indices, sort=sort, results=results)
 
 
