7f6d908b73aae95daad37b71603cbf5f170fdf72
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-04 18:48:42 +0100

    Add database core for users

--- solr_entrypoint.sh
+++ solr_entrypoint.sh
@@ -3,4 +3,5 @@
 precreate-core stable /owdex_configset
 precreate-core unstable /owdex_configset
 precreate-core archive /owdex_configset
+precreate-core users /owdex_configset_users
 /opt/solr-9.1.1/bin/solr start -f
