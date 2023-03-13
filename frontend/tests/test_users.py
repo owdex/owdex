e45b0d7a889a641c28fbfc6f3e8d4ee6763f9bf9
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-13 17:37:59 +0100

    Make tests more specific

--- frontend/tests/test_users.py
+++ frontend/tests/test_users.py
@@ -3,7 +3,7 @@ import owdex.users
 from owdex.usermanager import UserManager
 
 
-def test_users(client):
+def test_signup_login(client):
     with client:
         create_response = client.post("/signup",
                                       data={
@@ -20,10 +20,28 @@ def test_users(client):
         assert login_response.status_code == 200
         assert f.session["user"] == "test@test.test"
 
-        protected_response = client.get("/protected")
-        assert protected_response.status_code == 200
-        assert f.session["user"] == "test@test.test"
 
-        logout_response = client.get("/logout")
-        assert logout_response.status_code == 200
-        assert "user" not in f.session
+def test_create_user(client):
+    with client:
+        create_response = client.post("/signup",
+                                      data={
+                                          "email": "test@test.test",
+                                          "password": "test"
+                                      })
+        assert create_response.status_code == 200
+
+
+def test_access_protected_area_authenticated(client):
+    with client.session_transaction() as session:
+        session["user"] = "test@test.test"
+    protected_response = client.get("/protected")
+    assert protected_response.status_code == 200
+
+
+def test_logout(client):
+    with client.session_transaction() as session:
+        session["user"] = "test@test.test"
+    logout_response = client.get("/logout")
+    assert logout_response.status_code == 200
+    with client.session_transaction() as session:
+        assert "user" not in session
