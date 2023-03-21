0dba4a06381a3b5956708a55972d6aebb1a5b485
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-21 19:36:38 +0100

    Add decorators to require login

--- frontend/owdex/users.py
+++ frontend/owdex/users.py
@@ -1,11 +1,29 @@
+from functools import wraps, partial
+
 import flask as f
 from argon2.exceptions import VerifyMismatchError
 
-users = f.Blueprint('users', __name__, template_folder="templates")
+users = f.Blueprint("users", __name__, template_folder="templates")
 
 
 def get_current_user():
     return f.current_app.um.get(f.session['user'])
+
+
+def require_login(endpoint=None, needs_admin=False):
+    if endpoint is None:
+        return partial(require_login, needs_admin=needs_admin)
+
+    @wraps(endpoint)
+    def wrapper(*args, **kwargs):
+        if "user" not in f.session:
+            return "Not logged in!", 401
+        elif needs_admin and not get_current_user()["admin"]:
+            return "Not an admin!", 403
+        else:
+            return endpoint(*args, **kwargs)
+
+    return wrapper
 
 
 @users.route("/login", methods=["GET", "POST"])
@@ -52,19 +70,12 @@ def logout():
 
 
 @users.route("/protected")
+@require_login
 def protected():
-    if "user" in f.session:
-        return f"Logged in as {get_current_user()}"
-    else:
-        return "Not logged in!", 401
+    return f"Logged in as {get_current_user()}"
 
 
 @users.route("/admin")
+@require_login(needs_admin=True)
 def admin():
-    if "user" in f.session:
-        if get_current_user()["admin"]:
-            return f.render_template("admin.html")
-        else:
-            return "Not an admin!", 403
-    else:
-        return "Not logged in!", 401
+    return f.render_template("admin.html")
