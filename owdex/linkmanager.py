4e5c9ebeb005c3388e118f62cd7fda074e284f9a
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-07 01:32:17 +0200

    Use requests library

--- owdex/linkmanager.py
+++ owdex/linkmanager.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 
 import pysolr
 
-from urllib import request
+from requests import get
 from bs4 import BeautifulSoup as bs
 from url_normalize import url_normalize
 
@@ -35,26 +35,23 @@ class LinkManager:
             submitter (str, optional): The person who submitted the entry. Defaults to None. Should it equal None, it will be replaced with ANONYMOUS_SUBMITTER from owdex.toml.
         """
         # We force arguments to be named for readability by using *
-        url = url_normalize(url)
 
-        submitter = submitter if submitter \
-            else app.config["ANONYMOUS_SUBMITTER"]
+        submitter = submitter if submitter else app.config["ANONYMOUS_SUBMITTER"]
         # this can't be the default param because current_app isn't available initially
 
-        with request.urlopen(url) as response:
-            soup = bs(response.read(), features="html.parser")
-            content = soup.get_text()
-            description = soup.find("meta", attrs={"name": "description"})
+        soup = bs(get(url_normalize(url)).text, features="html.parser")
+        content = soup.get_text()
+        description = soup.find("meta", attrs={"name": "description"})
 
-            # if there was a description, set that, otherwise just use content
-            description = description.get(
-                "content") if description else content
+        # if there was a description, set that, otherwise just use content
+        description = description.get(
+            "content") if description else content
 
-            if len(description) > app.config["DESCRIPTION_MAX_LENGTH"]:
-                description = description[:app.
-                                          config["DESCRIPTION_MAX_LENGTH"] -
-                                          1] + "&hellip;"
-                # we subtract 1 extra so we have space to add the ellipsis
+        if len(description) > app.config["DESCRIPTION_MAX_LENGTH"]:
+            description = description[:app.
+                                        config["DESCRIPTION_MAX_LENGTH"] -
+                                        1] + "&hellip;"
+            # we subtract 1 extra so we have space to add the ellipsis
 
         self._indices[index].add(
             {
@@ -87,7 +84,7 @@ class LinkManager:
             indices (str): The names of the indices in which to search.
 
         Returns:
-            list: A list of result dicts.
+            list: A list of         response = result dicts.
         """
         results = []
         for index_name in indices:
@@ -101,9 +98,9 @@ class LinkManager:
 
 
 def get_title(url, format_for_autocomplete=False):
-    with request.urlopen(url_normalize(url)) as response:
-            return bs(
-                response.read(), features="html.parser"
-            ).find(
+    return bs(
+                get(url_normalize(url)).text, 
+                features="html.parser"
+             ).find(
                 "title"
-            ).text
+             ).text
