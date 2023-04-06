43c76564740923e5b79de69d0758bb716f93002e
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-06 23:12:31 +0200

    Handle errors more nicely with http module

--- owdex/error.py
+++ owdex/error.py
@@ -1,7 +1,11 @@
 import flask as f
+from http import HTTPStatus
 
 def error(status, explanation=None):
-	return f.render_template("error.html", status=status, explanation=explanation), status
+	# convert status to an HTTPStatus if it isn't already
+	status = [status_obj for status_obj in HTTPStatus if status_obj.value == status][0] if isinstance(status, int) else status
+	
+	return f.render_template("error.html", status=status.value, phrase=status.phrase, explanation=explanation), status.value
 
 def page_not_found(e):
 	return error(404, explanation="Page not found!")
