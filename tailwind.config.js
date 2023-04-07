2494e6102f95fe49189d1bada5df4727be2ad26c
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-07 16:46:49 +0200

    Use macros

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: {
     relative: true,
     files: [
-      "./*.html"
+      "./**/*.html"
       // this looks in the source directory, because it is run in a Docker env where ./owdex/templates/* is copied next to it
     ]
   },
